9ae768c189116737d08c5b0606cb0af9663e6d50
marreduder <marios@anmo.se>
2025-01-21 20:27:32 +0100

    Create lead-organizer.tsx

---
+++ components/lead-organizer.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import React, { useState } from 'react';
+import Papa from 'papaparse';
+import { Upload, BarChart2, Filter } from 'lucide-react';
+
+// [Rest of the component code here - would you like me to show it?]
