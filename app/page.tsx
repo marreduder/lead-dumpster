cb27e8b55b7b0bfc9e746468d573123f3f6cc72b
marreduder <marios@anmo.se>
2025-01-21 20:27:11 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import LeadOrganizer from '../components/lead-organizer';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-8">
+      <LeadOrganizer />
+    </main>
+  );
+}
