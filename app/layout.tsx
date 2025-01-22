cc69afa8a136124e52cc52d28a42b942ec2ca147
marreduder <marios@anmo.se>
2025-01-22 19:17:17 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,11 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "LeadDumpster",
+  description: "Lead organization tool",
+};
+
 export default function RootLayout({
   children,
 }: {
